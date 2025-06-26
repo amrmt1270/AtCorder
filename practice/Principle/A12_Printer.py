a4af460b7449b9644040f8f87a07c0b656b4577b
Aki Fukuda <akif.3839@keio.jp>
2025-06-26 12:53:17 +0900

    A16-Dungeon1.py

--- practice/Principle/A12_Printer.py
+++ practice/Principle/A12_Printer.py
@@ -1,3 +1,22 @@
 N, K = map(int, input().split())
 
 A = list(map(int, input().split()))
+#x 秒で実現可能か？
+def check(x, N, K, A):
+    count= 0
+    for i in range(N):
+        count += x //A[i]
+    if count >= K :
+        return True
+    return False
+#leftは常に実現不可能な値
+left = 0
+#rightは常に実現可能な値
+right = 10 ** 9
+while right > left:
+    mid = (left + right)//2
+    if check(mid, N,K, A) == True:
+        right = mid
+    if check(mid, N, K, A) == False:
+        left = mid + 1
+print(left)
