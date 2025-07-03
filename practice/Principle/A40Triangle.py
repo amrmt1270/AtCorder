50f94588e18282ac4eae9acb89793fe08fd14f7c
amrmt1270 <akif.3839@keio.jp>
2025-07-03 19:06:28 +0900

    A40 completed

--- practice/Principle/A40Triangle.py
+++ practice/Principle/A40Triangle.py
@@ -1,3 +1,15 @@
+import math
+
 N = int(input())
 A = list(map(int, input().split()))
 
+total = 0
+for i in range(max(A) + 1) :
+    m = A.count(i)
+    if m <= 2 :
+        pass
+    else :
+        triangle = int(math.factorial(m) / (6 * math.factorial(m-3)))
+        total += triangle
+    
+print(total)
