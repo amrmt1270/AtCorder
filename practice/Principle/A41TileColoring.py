468c2a760589c52c5c2a8daaf5f23432f6667159
Aki Fukuda <akif.3839@keio.jp>
2025-07-07 11:58:44 +0900

    A41 Completed

--- practice/Principle/A41TileColoring.py
+++ practice/Principle/A41TileColoring.py
@@ -1,3 +1,14 @@
 N = int(input())
-s = list(str(input()))
+s = input()
 
+answer = False
+for i in range(0, N-2) :
+    if s[i] == 'R' and s[i+1] == 'R' and s[i+2] == 'R' :
+        answer = True
+    if s[i] == 'B' and s[i+1] == 'B' and s[i+2] == 'B' :
+        answer = True
+
+if answer == True :
+    print('Yes')
+else:
+    print('No')
